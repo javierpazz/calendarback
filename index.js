06678c5823792699316f89b1d1a8e0fee351622c
JAVIER <javierpia0105@gmail.com>
2023-03-06 14:48:06 -0300

    7 commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-//const cors = require('cors');
+const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
