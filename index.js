96bebbed6bccc0c8e9a8c040d4652105ee913c4c
JAVIER <javierpia0105@gmail.com>
2023-03-06 17:33:44 -0300

    first commit

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ const app = express();
 // Base de datos
 dbConnection();
 
+//Definir dominios para recibir las peticiones
+const whitelist = ['http://localhost:3000',' https://javierpazz.github.io/calendarfront'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Recisar si la peticion viene de un servidor que esta en la whitelist
+    const existe = whitelist.some((dominio) => dominio === origin);
+    if (existe) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por Cors'));
+    }
+  },
+};
+
 // CORS
-//app.use(cors())
+app.use(cors(corsOptions));
 
 // Directorio Público
 app.use(express.static('public'));
