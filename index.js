2c937ad117358ce5050353ae28fb21036bb18f7d
JAVIER <javierpia0105@gmail.com>
2023-03-06 14:37:25 -0300

    6 commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const cors = require('cors');
+//const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
 // Crear el servidor de express
@@ -10,28 +10,19 @@ const app = express();
 dbConnection();
 
 // CORS
-app.use(cors())
+//app.use(cors())
 
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 // Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
-
-
-
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
 
 // Escuchar peticiones
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
-
-
-
-
-
-
