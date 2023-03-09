2b6650e296f90f6eb7c24f8b46b2ed2b9a3f75cf
JAVIER <javierpia0105@gmail.com>
2023-03-09 11:10:03 -0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const psth = require('path');
+
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
@@ -22,8 +24,14 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+//const __dirname = path.resolve();
+//app.use(express.static(path.join(__dirname, '/public')));
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname + '/public/index.html'));
+//});
+
 app.get('*', (req, res) => {
-  res.sendFile( __dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // Escuchar peticiones
